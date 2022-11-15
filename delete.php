087a7149d12c462f83c9cf7a8410fb3f66cb8572
geonoo <yung65322136@gmail.com>
2022-11-15 15:48:41 +0900

    #3
    글삭제

--- delete.php
+++ delete.php
@@ -4,28 +4,29 @@ $conn = mysqli_connect(
     'root',
     '12345678',
     'opentutorials');
+
 $sql = "SELECT * FROM topic";
 $result = mysqli_query($conn, $sql);
 $list = '';
 while($row = mysqli_fetch_array($result)) {
-    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+    $escaped_title = htmlspecialchars($row['title']);
+    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$escaped_title}</a></li>";
 }
+
 $article = array(
     'title'=>'Welcome',
     'description'=>'Hello, web'
 );
 $update_link = '';
-$delete_link = '';
 if(isset($_GET['id'])) {
     $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
     $sql = "SELECT * FROM topic WHERE id={$filtered_id}";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result);
-    $article['title'] = $row['title'];
-    $article['description'] = $row['description'];
+    $article['title'] = htmlspecialchars($row['title']);
+    $article['description'] = htmlspecialchars($row['description']);
 
-    $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a> ';
-    $delete_link = '<a href="delete.php?id='.$_POST['id'].'">delete</a>';
+    $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
 }
 
 ?>
@@ -40,10 +41,11 @@ if(isset($_GET['id'])) {
 <ol>
     <?=$list?>
 </ol>
-<a href="create.php">create</a>
-<?=$update_link?>
-<?=$delete_link?>
-<h2><?=$article['title']?></h2>
-<?=$article['description']?>
+<form action="process_update.php" method="POST">
+    <input type="hidden" name="id" value="<?=$_GET['id']?>">
+    <p><input type="text" name="title" placeholder="title" value="<?=$article['title']?>"></p>
+    <p><textarea name="description" placeholder="description"><?=$article['description']?></textarea></p>
+    <p><input type="submit"></p>
+</form>
 </body>
 </html>
